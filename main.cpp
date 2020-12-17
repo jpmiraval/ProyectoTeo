c20b645a4c1fd031f7a3d8864d9b4cb8c6ad28ce
jpmiraval <40766473+jpmiraval@users.noreply.github.com>
2020-12-17 07:55:35 -0500

    Update Main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include "functions.h"
 
 int main(){
-    brzozowski();
-    std::cout << "\n \nEq\n";
-    auto afd = read_AFD();
-    Eq_test(*afd);
 
+
+   //brzozowski();
+
+
+    AFD *afd = read_AFD();
+    AFD afd_bu = *afd;
+
+    std::cout << "Sin optimizar\n\n";
+    Eq_test(*afd);
+    *afd = afd_bu;
+    std::cout << "\nOptimizado\n\n";
+    Eq_test_op(*afd);
+    *afd = afd_bu;
+    auto answer_5 = Hopcroft(*afd);
+    *afd = afd_bu;
+    auto answer_4 = Huffman_Moore(*afd);
+    *afd = afd_bu;
+    std::cout << "\n\nHuffman\n\n";
+    answer_4 ->printAFD();
+    std::cout << "\n\nHopcroft\n\n";
+    answer_5 ->printAFD();
     return EXIT_SUCCESS;
 }
